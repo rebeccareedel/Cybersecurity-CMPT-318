5bd690618a245621c127644b7cb37892c8f8859e
rmr6 <rmr6@sfu.ca>
2023-09-25 14:48:29 -0700

    Added #1, instructions

--- Assignment1/Assignment1.R
+++ Assignment1/Assignment1.R
@@ -1,1 +1,15 @@
-b bbn
+# Group Project #24, Rebecca Reedel 301454910, Asmita Srivastava, Mrinal Goshalia
+
+# read dataset file
+table = read.table(Group_Assignment_1_Dataset, header = );
+
+#extract data spanning one full week from Monday to Sunday. 
+#The week assigned to your group is determined by your group number
+
+# 1. compute arithmetic and geometric mean, median, mode and standard dev. 
+#for A, B, C features. For features A and B compute the min and max
+#values on weekdays and weekend days during day hours and night hours 
+
+
+#In order to extract specific days from a time series you will need this command:
+  #as.POSIXlt(date, format = ""
