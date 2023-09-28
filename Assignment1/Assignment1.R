3d384e85481a29d17532ec92b0351be5422d4abb
rmr6 <rmr6@sfu.ca>
2023-09-28 13:37:44 -0700

    Added function for mode

--- Assignment1/Assignment1.R
+++ Assignment1/Assignment1.R
@@ -1,8 +1,14 @@
 # Group Project #24, Rebecca Reedel 301454910, Asmita Srivastava, Mrinal Goshalia
 
+# mode function adapted from https://statisticsglobe.com/mode-in-r-programming-example
+mode <- function(x) {                   
+  unique_x <- unique(x)
+  tabulate_x <- tabulate(match(x, unique_x))
+  unique_x[tabulate_x == max(tabulate_x)]
+}
+
 # read dataset file
 data = read.table('Group_Assignment_1_Dataset.txt', header =TRUE, sep =',' )
-print(data)
 
 #extract data spanning one full week from Monday to Sunday. 
 #The week assigned to your group is determined by your group number
@@ -14,37 +20,37 @@ print(data)
 A_a_mean <-mean(data$Global_active_power)
 A_g_mean <-exp(mean(log(data$Global_active_power)))
 A_median <-median(data$Global_active_power)
-#A_mode <-mode(data$Global_active_power) no built in 
+A_mode <-mode(data$Global_active_power)
 A_std <-sd(data$Global_active_power)
 
 B_a_mean <-mean(data$Global_reactive_power, na.rm=TRUE)
 B_g_mean <-exp(mean(log(data$Global_reactive_power), na.rm=TRUE))
 B_median <-median(data$Global_reactive_power, na.rm=TRUE)
-#B_mode <-mode(data$Global_reactive_power, na.rm=TRUE) no built in 
+B_mode <-mode(data$Global_reactive_power)  
 B_std <-sd(data$Global_reactive_power, na.rm=TRUE)
 
 C_a_mean <-mean(data$Voltage, na.rm=TRUE)
 C_g_mean <-exp(mean(log(data$Voltage), na.rm=TRUE))
-C_median <-median(data$Voltage, na.rm=TRUE)
-#C_mode <-mode(data$Voltage, na.rm=TRUE) no built in 
+C_median <-median(data$Voltage)
+C_mode <-mode(data$Voltage) 
 C_std <-sd(data$Voltage, na.rm=TRUE)
 
 print(A_a_mean)
 print(A_g_mean)
 print(A_median)
-#print(A_mode)
+print(A_mode)
 print(A_std)
 
 print(B_a_mean)
 print(B_g_mean)
 print(B_median)
-#print(B_mode)
+print(B_mode)
 print(B_std)
 
 print(C_a_mean)
 print(C_g_mean)
 print(C_median)
-#print(C_mode)
+print(C_mode)
 print(C_std)
 
 #For features A and B compute the min and max
