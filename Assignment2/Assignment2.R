c9370171a2daad4a154f3066496f4dd938256578
Mrinal G <mrinal_goshalia@sfu.ca>
2023-10-18 16:35:55 -0700

    Update Assignment2.R

--- Assignment2/Assignment2.R
+++ Assignment2/Assignment2.R
@@ -1,10 +1,21 @@
 #CMPT318 Assignment 2
+library(dplyr)
+library(lubridate)
+library(zoo)
 
 #PART 2
 data = read.table('Group_Assignment_1_Dataset.txt', header =TRUE, sep =',') 
 data$Date <- as.Date(data$Date, '%d/%m/%Y')
 data$Week = strftime(data$Date, format = "%V")
+#print(data)
+
+# Calculate the moving average for Global_intensity
+data <- data %>%
+  group_by(Week) %>%
+  mutate(Moving_Avg_Global_intensity = rollmean(Global_intensity, k = 7, fill = NA))
+
 print(data)
 
-weekly_data = aggregate(Global_intensity  ~ Week, data, sum)
+weekly_data <- aggregate(Moving_Avg_Global_intensity ~ Week, data, FUN = mean, na.rm = TRUE)
 print(weekly_data)
+
