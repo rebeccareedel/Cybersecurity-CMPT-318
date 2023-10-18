fd5a0d327bdbe9bdfb8231a11b04fdca7c109dd4
Asmita Srivastava <asmita_srivastava@sfu.ca>
2023-10-18 16:11:36 -0700

    Update Assignment2.R

--- Assignment2/Assignment2.R
+++ Assignment2/Assignment2.R
@@ -2,5 +2,9 @@
 
 #PART 2
 data = read.table('Group_Assignment_1_Dataset.txt', header =TRUE, sep =',') 
+data$Date <- as.Date(data$Date, '%d/%m/%Y')
+data$Week = strftime(data$Date, format = "%V")
+print(data)
 
-
+weekly_data = aggregate(Global_intensity  ~ Week, data, sum)
+print(weekly_data)
