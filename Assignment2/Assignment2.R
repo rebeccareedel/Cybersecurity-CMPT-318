103919091baef9fdf96c233e7340dcc954faf11c
rmr6 <rmr6@sfu.ca>
2023-10-18 17:53:41 -0700

    Added anomaly and plotting

--- Assignment2/Assignment2.R
+++ Assignment2/Assignment2.R
@@ -3,19 +3,40 @@ library(dplyr)
 library(lubridate)
 library(zoo)
 
-#PART 2#
+#PART 2
 data = read.table('Group_Assignment_1_Dataset.txt', header =TRUE, sep =',') 
 data$Date <- as.Date(data$Date, '%d/%m/%Y')
 data$Week = strftime(data$Date, format = "%V")
-#print(data)
 
-# Calculate the moving average for Global_intensity
+# calculate the moving average to get smoothed week for Global_intensity
 data <- data %>%
   group_by(Week) %>%
   mutate(Moving_Avg_Global_intensity = rollmean(Global_intensity, k = 7, fill = NA))
 
-print(data)
-
+# calculate average smoothed week
 weekly_data <- aggregate(Moving_Avg_Global_intensity ~ Week, data, FUN = mean, na.rm = TRUE)
-print(weekly_data)
+-
+# calculates anomaly score for every week
+data$AnomalyScore <- abs(data$Moving_Avg_Global_intensity - weekly_data$Moving_Avg_Global_intensity[match(data$Week, weekly_data$Week)])
+
+# create anomaly score table
+anomaly_table <- data.frame(Week = data$Week, AnomalyScore = data$AnomalyScore)
+anomaly_table <- na.omit(anomaly_table)
+
+# get week with highest anomaly score and the least anomaly score
+most_anomalous_week <- subset(anomaly_table, AnomalyScore == max(anomaly_table$AnomalyScore))
+least_anomalous_week <- subset(anomaly_table, AnomalyScore == min(anomaly_table$AnomalyScore))
+
+# get highest and lowest anomaly score week data for graphing
+most_anomalous_data <- data[data$Week == most_anomalous_week$Week, ]
+least_anomalous_data <- data[data$Week == least_anomalous_week$Week, ]
+
+# plot the data
+ggplot(data, aes(x = Date, y = Moving_Avg_Global_intensity)) +
+  geom_line(aes(color = "Average Smoothed Week")) +
+  geom_line(data = most_anomalous_data, aes(x = Date, y = Moving_Avg_Global_intensity, color = "Most Anomalous Week")) +
+  geom_line(data = least_anomalous_data, aes(x = Date, y = Moving_Avg_Global_intensity, color = "Least Anomalous Week")) +
+  scale_color_manual(values = c("Average Smoothed Week" = "blue", "Most Anomalous Week" = "red", "Least Anomalous Week" = "green")) +
+  labs(title = "Smoothened Weeks Comparison", y = "Global Intensity") +
+  theme_minimal()
 
