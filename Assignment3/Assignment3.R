2090025172fabff9a2a1dc9c7efb24857f947daa
Asmita Srivastava <asmita_srivastava@sfu.ca>
2023-10-30 14:24:45 -0700

    Update Assignment3.R

--- Assignment3/Assignment3.R
+++ Assignment3/Assignment3.R
@@ -3,19 +3,27 @@
 install.packages("depmixS4")
 library(depmixS4)
 library(tidyverse)
+library(dplyr)
+
 
 data = read.table('Group_Assignment_1_Dataset.txt', header =TRUE, sep =',') 
-
-#TODO: scale data, data needs to be numeric first
-data$Global_reactive_power <- as.numeric(data$Global_active_power)
-scaled_data <- scale(data$Global_reactive_power)
-
+print(data)
+data$Time = as.POSIXct(data$Time, format = '%H:%M:%S')
 data$week = strftime(data$Date, format = "%a")
+data = na.omit(data)
+#TODO: scale data, data needs to be numeric first
+#data$Global_reactive_power <- as.numeric(data$Global_active_power)
+#scaled_data <- scale(data$Global_reactive_power)
+data[] = lapply(data, as.numeric)
+scaled_data = scale(data, center = TRUE, scale = TRUE)
+print(scaled_data)
+
+
 # Group data by time windows and calculate mean of Global Intensity
-data = data %>%
+grouped_data = scaled_data %>%
   group_by(week, Time)
 
-print(data)
+print(grouped_data)
 n = rep(c(120),each=52)
 model <- depmix(response = Average_Global_intensity, data = grouped_data, n_states = n)
 
