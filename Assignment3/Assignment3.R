139120f1304cbeedb69189317e67a8307162943a
rmr6 <rmr6@sfu.ca>
2023-10-30 14:37:42 -0700

    Added note outline

--- Assignment3/Assignment3.R
+++ Assignment3/Assignment3.R
@@ -1,4 +1,4 @@
-#CMPT318 Assignment 3
+# CMPT318 Assignment 3
 # Authors: Mrinal Goshalia, Rebecca Reedel, Asmita Srivastava
 install.packages("depmixS4")
 library(depmixS4)
@@ -18,6 +18,26 @@ data = na.omit(data)
 data[] = lapply(data, as.numeric)
 scaled_data = scale(data, center = TRUE, scale = TRUE)
 print(scaled_data)
+
+# determine a time window for a specific weekday that shows a
+#clearly recognizable electricity consumption pattern over a time period  >120 &<240 minutes. 
+# for global_reactive_power
+
+
+# Extract the same time window for each week of the dataset
+
+# concatenate the extracted time windows to build a dataset for the training of HMMs
+
+# train a number of univariate HMMs, with number of states >=3, <=16
+# NOTE: You may not need to train HMMs for each and every number of states within the range by
+# making smart choices.
+
+# For each HMM, compute the log-likelihood measure on the training dataset
+
+# for each HMM, compute the Bayesian information criterion (BIC) = measure of complexity of model
+
+# goal is to find the intercept of the two plots for log-likelihood
+# and BIC values respectively so as to determine the best model (avoiding overfitting).
 
 n = rep(c(120),each=52)
 model <- depmix(response = Average_Global_intensity, data = grouped_data, n_states = n)
