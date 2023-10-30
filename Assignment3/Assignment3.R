9c7bc7bc9cd9ebf26979d1e7756bb91680ceafdb
rmr6 <rmr6@sfu.ca>
2023-10-30 14:49:59 -0700

    added note

--- Assignment3/Assignment3.R
+++ Assignment3/Assignment3.R
@@ -11,7 +11,7 @@ data = read.table('Group_Assignment_1_Dataset.txt', header =TRUE, sep =',')
 # convert data to be numeric
 data$Date = as.Date(data$Date, '%d/%m/%Y')
 data$Time = as.POSIXct(data$Time, format = '%H:%M:%S')
-data$week = strftime(data$Date, format = "%a")
+data$Week = strftime(data$Date, format = "%a")
 data = na.omit(data)
 
 # scale data
@@ -35,6 +35,7 @@ print(scaled_data)
 # For each HMM, compute the log-likelihood measure on the training dataset
 
 # for each HMM, compute the Bayesian information criterion (BIC) = measure of complexity of model
+# NOTE: want highest log-like and lowest BIC
 
 # goal is to find the intercept of the two plots for log-likelihood
 # and BIC values respectively so as to determine the best model (avoiding overfitting).
