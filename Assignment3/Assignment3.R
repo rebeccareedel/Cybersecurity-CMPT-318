274d108cc24c3109e7bae32c45047e6da65deebf
Asmita Srivastava <asmita_srivastava@sfu.ca>
2023-11-01 21:51:22 -0700

    Update Assignment3.R

--- Assignment3/Assignment3.R
+++ Assignment3/Assignment3.R
@@ -80,11 +80,12 @@ model_data <- data.frame(Model = 1:4, BIC = BIC_values, LogLik = LLH_values)
 
 # Plot BIC and log-likelihood values
 ggplot(model_data, aes(x = Model)) +
-  geom_line(aes(y = BIC, color = "BIC"), linetype = "solid", size = 0.3) +
-  geom_line(aes(y = LogLik, color = "Log-Likelihood"), linetype = "dashed", size = 0.3) +
+  geom_line(aes(y = BIC, color = "BIC"), linetype = "solid", size = 0.5) +
+  geom_line(aes(y = LogLik, color = "Log-Likelihood"), linetype = "solid", linewidth = 0.5) +
   scale_color_manual(values = c("BIC" = "darkblue", "Log-Likelihood" = "orange")) +
   labs(x = "Model", y = "Values") +
   theme_minimal() +
   ggtitle("BIC and Log-Likelihood Model Comparisons") +
   theme(plot.title = element_text(face = "bold")) +
-  geom_vline(xintercept = best_model_index, linetype = "dotted", color = "turquoise", size = 1)
+  geom_vline(xintercept = best_model_index, linetype = "dotted", color = "turquoise", linewidth = 1)
+ 
