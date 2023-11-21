be6b7cccfb779765a9a8523a8a3800bbe8a328ed
rmr6 <rmr6@sfu.ca>
2023-11-21 10:27:12 -0800

    Added part 1

--- Term Project/termproject.R
+++ Term Project/termproject.R
@@ -8,3 +8,15 @@ library(ggplot2)
 
 # read table
 data = read.table('TermProjectData.txt', header =TRUE, sep =',') 
+
+# PART 1. FEATURE ENGINEERING
+
+# scale the raw data using standardization prior to applying PCA.
+
+# For deciding on the subset of variables that are most suitable for training your models,
+# you need to perform a Principal Component Analysis (PCA)
+
+# Choose a subset of the response variables for training of multivariate
+# HMMs on normal electricity consumption data. 
+
+# Provide a 1 proper rational for your final choice of response variables based on your PCA results. 
