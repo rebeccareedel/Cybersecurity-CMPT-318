5c33d152fbb7419eb228bdc2c598412fcac70505
Asmita Srivastava <asmita_srivastava@sfu.ca>
2023-11-23 15:53:50 -0800

    Update part 2 code

--- Term Project/termproject.R
+++ Term Project/termproject.R
@@ -50,9 +50,15 @@ subset = scaled_data[c("Date", "Time", "weekday", "Global_active_power", "Global
 # HMMs on normal electricity consumption data. 
 
 # Provide a 1 proper rational for your final choice of response variables based on your PCA results. 
-#PC1 + PC2 = 70% of data representation (approximately), plus choosing PC3
+#PC1 + PC2 = 70% of data representation (approximately), plus choosing PC3 = 90% of all the data representation.
 
 # PART 2. HMM TRAINING AND TESTING
+
+# Choose a weekday or a weekend day and a time window between 2 to 6 hours on that day. 
+data_monall = subset[subset$weekday == 'Mon',]
+time_window = data_monall[data_monall$Time >= "2023-11-23 10:00:00" & data_monall$Time < "2023-11-23 14:00:00", ]
+time_window = subset(time_window, select = c(Time, Global_reactive_power, Voltage)) # MODIFY WITH PCA RESP VARIABLES
+time_window = na.omit(time_window)
 
 # Partition your scaled data into train and test. 
 #Test-Train-Split Code adapted from https://www.statology.org/train-test-split-r/
@@ -60,12 +66,6 @@ subset = scaled_data[c("Date", "Time", "weekday", "Global_active_power", "Global
 sample <- sample(c(TRUE, FALSE), nrow(subset), replace=TRUE, prob=c(0.6,0.4))
 train  <- subset[sample, ]
 test   <- subset[!sample, ]
-
-# Choose a weekday or a weekend day and a time window between 2 to 6 hours on that day. 
-data_monall = train[train$weekday == 'Mon']
-time_window = data_monall[data_monall$Time >= "2023-11-01 10:00:00" & data_monall$Time < "2023-11-01 14:00:00", ]
-time_window = subset(time_window, select = c(Time, Global_reactive_power)) # MODIFY WITH PCA RESP VARIABLES
-time_window = na.omit(time_window)
 
 # Partition your scaled data into train and test. 
 
